ced0d475a59c201f34e8b5cee0447055ecb5a479
JonasKoerber <JonasKoerber@t-online.de>
2022-06-25 13:45:52 +0200

    sort order basic functions

--- web-app/src/app/components/items/drag-drop/drag-drop.component.ts
+++ web-app/src/app/components/items/drag-drop/drag-drop.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {CdkDragDrop, moveItemInArray} from '@angular/cdk/drag-drop';
+
 
 @Component({
   selector: 'app-drag-drop',
@@ -12,50 +12,6 @@ export class DragDropComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-  }
-
-
-  questiondata = {
-    "questionId": 0,
-    "questionType": 1,
-    "category": 0,
-    "questionText": "",
-    "imageUrl": "",
-    "tip":"",
-    "answeredCorrect":false,
-    "givenAnswers":["answer1","answer2"],
-    
-    "additonalInfos":{
-        "options": ["", ""],
-        "correctAnswer": [
-            {
-                "name": "",
-                "value": ""
-            },
-            {
-                "name": "",
-                "value": ""
-            }
-        ]
-    }
-}
-
- 
-
-  movies = [
-    'Episode I - The Phantom Menace',
-    'Episode II - Attack of the Clones',
-    'Episode III - Revenge of the Sith',
-    'Episode IV - A New Hope',
-    'Episode V - The Empire Strikes Back',
-    'Episode VI - Return of the Jedi',
-    'Episode VII - The Force Awakens',
-    'Episode VIII - The Last Jedi',
-    'Episode IX – The Rise of Skywalker',
-  ];
-
-  drop(event: CdkDragDrop<string[]>) {
-    moveItemInArray(this.movies, event.previousIndex, event.currentIndex);
   }
 
 
