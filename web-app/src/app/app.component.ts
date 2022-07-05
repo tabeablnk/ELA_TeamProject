bcafb457cb2981779d3bdde6614467fea76a2ef3
tabeablnk <tabeablenk@gmail.com>
2022-07-05 17:44:03 +0200

    navbar + navigation

--- web-app/src/app/app.component.ts
+++ web-app/src/app/app.component.ts
@@ -1,18 +1,53 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Category } from './models/state-enum.model';
 import { LoginGuardService } from './services/login-guard.service';
-import { UserService } from './services/user.service';
+import { StateService } from './services/state.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'web-app';
 
   loggedIn:any; 
 
-  constructor(private loginGuard: LoginGuardService, ){
+  constructor(private loginGuard: LoginGuardService, private router: Router, private state: StateService){
     this.loggedIn = loginGuard.canActivate(); 
+  }
+
+  ngOnInit(): void {
+    let url = window.location.href;
+    if(url.includes("category")) {
+      let currentCategory: Number = +url.charAt(url.length - 1);
+      switch (currentCategory) {
+        case 1:  
+          this.state.setCategory(Category.Demografie);
+          break;
+        case 2:
+          this.state.setCategory(Category.Kultur);
+          break;
+        case 3:
+          this.state.setCategory(Category.Geographie);
+          break;
+        case 4:
+          this.state.setCategory(Category.Geschichte);
+          break;
+
+          
+      }
+     
+    }
+    
+  }
+
+  openDashboard() {
+
+  }
+
+  openHome() {
+    this.router.navigate(['/home']);
   }
 }
