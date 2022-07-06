71aa5c2bfc2f8c5130341d723f54813bc67470b7
tabeablnk <tabeablenk@gmail.com>
2022-07-06 22:04:39 +0200

    optimized Tim's code

--- web-app/src/app/app.component.ts
+++ web-app/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Category } from './models/state-enum.model';
+import { CategoryQuestionsService } from './services/category-questions.service';
 import { LoginGuardService } from './services/login-guard.service';
 import { StateService } from './services/state.service';
 
@@ -14,11 +15,33 @@ export class AppComponent implements OnInit{
 
   loggedIn:any; 
 
-  constructor(private loginGuard: LoginGuardService, private router: Router, private state: StateService){
+  constructor(private loginGuard: LoginGuardService, private router: Router, private state: StateService, private categoryQuestions: CategoryQuestionsService){
     this.loggedIn = loginGuard.canActivate(); 
   }
 
   ngOnInit(): void {
+    //diese Methode wird am Anfang aufgerufen, am besten du sendest hier schon mal die SPARQUL Anfrage mit fetch
+    //über den categoryQuestinoService kannst du dann Fragen zu den verschiedenen QuestionsSets hinzufügen, wie hier zum Beispiel eine Single-Choice Frage für die Kategorie Demografie z.B.
+
+    //create new Question
+    // let new_question = {
+    //     questionId: 0,
+    //     questionType: 1,
+    //     questionTypeName: "SingleChoice",
+    //     category: 1,
+    //     questionText: "Wo liegt die Alte Mainbrücke?",
+    //     imageUrl: "",
+    //     tip: "Hier ein Tipp",
+    //     answeredCorrect: false,
+    //     givenAnswers: ["answer1", "answer2"],
+    
+    //     additionalInfos: {
+    //       options: ["München", "Würzburg", "Nürnberg", "Erlangen", "Hof"],
+    //       correctAnswer: "Würzburg"
+    //     }
+    // }
+    // this.categoryQuestions.addCategoryQuestion(Category.Demografie, new_question);
+
     let url = window.location.href;
     if(url.includes("category")) {
       let currentCategory: Number = +url.charAt(url.length - 1);
@@ -34,9 +57,7 @@ export class AppComponent implements OnInit{
           break;
         case 4:
           this.state.setCategory(Category.Geschichte);
-          break;
-
-          
+          break;          
       }
      
     }
--- web-app/src/app/services/category-questions.service.ts
+++ web-app/src/app/services/category-questions.service.ts
@@ -3,26 +3,72 @@ import questionsCategory_1 from '../../assets/questions/1.json';
 import questionsCategory_2 from '../../assets/questions/2.json';
 import questionsCategory_3 from '../../assets/questions/3.json';
 import questionsCategory_4 from '../../assets/questions/4.json';
+import { Category } from '../models/state-enum.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CategoryQuestionsService {
 
-  constructor() { }
+  questionSet_Kultur: any;
+  questionSet_Demografie: any;
+  questionSet_Geographie: any;
+  questionSet_Geschichte: any;
+
+  constructor() {
+    this.questionSet_Demografie = questionsCategory_1;
+    this.questionSet_Kultur = questionsCategory_2;
+    this.questionSet_Geographie = questionsCategory_3
+    this.questionSet_Geschichte = questionsCategory_4;
+  }
 
   getCategoryQuestions(categoryId: Number){
+    console.log(this.questionSet_Demografie);
     switch(categoryId){
       case 1:
-        return questionsCategory_1
+        return this.questionSet_Demografie
       case 2:
-        return questionsCategory_2
+        return this.questionSet_Kultur
       case 3: 
-        return questionsCategory_3
+        return this.questionSet_Geographie
       case 4: 
-        return questionsCategory_4
+        return this.questionSet_Geschichte
       default: 
         return "You have to set a category!";
+    }
+  }
+
+  addCategoryQuestion(category: Category, question: any) {
+    switch(category){
+      case Category.Demografie:
+        this.questionSet_Demografie.push(question);
+        break;
+      case 2:
+        this.questionSet_Kultur.push(question);
+        break;
+      case 3: 
+        this.questionSet_Geographie.push(question);
+        break;
+      case 4: 
+        this.questionSet_Geschichte.push(question);
+        break 
+    }
+  }
+
+  updateQuestionSet(category: Category, questionSet: any) {
+    switch(category){
+      case Category.Demografie:
+        this.questionSet_Demografie = questionSet;
+        break;
+      case 2:
+        this.questionSet_Kultur = questionSet
+        break;
+      case 3: 
+        this.questionSet_Geographie = questionSet
+        break;
+      case 4: 
+        this.questionSet_Geschichte = questionSet;
+        break 
     }
   }
 
