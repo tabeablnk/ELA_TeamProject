1a9cf99aa541d47ba3e090326c0e0f244b97ea81
Franziska Bumiler <franziska.bumiller@fau.de>
2022-07-12 14:02:01 +0200

    Added the code from the tryout to the app.component -> currently not working! (app can not import node-fetch)

--- web-app/src/app/app.component.ts
+++ web-app/src/app/app.component.ts
@@ -5,6 +5,26 @@ import { CategoryQuestionsService } from './services/category-questions.service'
 import { LoginGuardService } from './services/login-guard.service';
 import { StateService } from './services/state.service';
 
+// Import der fetch-API für SPARQL-Requests
+import fetch from 'node-fetch';
+
+
+class SPARQLQueryDispatcher {
+
+  endpoint: any;
+  constructor( endpoint: any ) {
+  this.endpoint = endpoint;
+}
+
+query( sparqlQuery: any ) {
+  const fullUrl = this.endpoint + '?query=' + encodeURIComponent( sparqlQuery );
+  const headers = { 'Accept': 'application/sparql-results+json' };
+
+  // Nutzung der fetch-API um die Anfrag an Wikidata zu schicken und die Reultate als JSON zurück zu erhalten
+  return fetch( fullUrl, { headers } ).then((body: any) => body.json());
+}
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -22,6 +42,50 @@ export class AppComponent implements OnInit{
   ngOnInit(): void {
     //diese Methode wird am Anfang aufgerufen, am besten du sendest hier schon mal die SPARQUL Anfrage mit fetch
     //über den categoryQuestinoService kannst du dann Fragen zu den verschiedenen QuestionsSets hinzufügen, wie hier zum Beispiel eine Single-Choice Frage für die Kategorie Demografie z.B.
+
+
+    // Endpoint von WikiData an den die Anfrage geschickt werden muss
+    var endpointUrl = 'https://query.wikidata.org/sparql';
+
+    // Hier entsprechend die Query als String einpflegen
+    var sparqlQuery = `#Bayrische Städte nach Anzahl der Einwohner (Top 100)
+    #defaultView:BubbleChart
+    SELECT DISTINCT ?city ?cityLabel ?population ?gps
+    WHERE
+    {
+      ?city wdt:P31/wdt:P279* wd:Q515 .
+      ?city wdt:P131*/wdt:P31*/wdt:P361*/wdt:P706* wd:Q980 .
+      ?city wdt:P1082 ?population .
+      ?city wdt:P625 ?gps .
+      SERVICE wikibase:label {
+        bd:serviceParam wikibase:language "de" .
+      }
+    }
+    ORDER BY DESC(?population) LIMIT 10`;
+
+    const queryDispatcher = new SPARQLQueryDispatcher( endpointUrl );
+
+    const prefixes_wikidata = 'PREFIX wdt: <http://www.wikidata.org/prop/direct/>\n'+ 
+              'PREFIX wd: <http://www.wikidata.org/entity/>\n' + 
+              'PREFIX wikibase: <http://wikiba.se/ontology#>\n' + 
+              'PREFIX bd: <http://www.bigdata.com/rdf#>\n';
+
+
+    // installed sparqljs with npm install sparqljs (s. Notion for links to documantation)
+    // Parse a SPARQL query to a JSON object
+    var SparqlParser = require('sparqljs').Parser;
+    var parser = new SparqlParser();
+    var parsedQuery = parser.parse(prefixes_wikidata + sparqlQuery);
+    //console.log(parsedQuery)
+
+    // Regenerate a SPARQL query from a JSON object
+    var SparqlGenerator = require('sparqljs').Generator;
+    var generator = new SparqlGenerator();
+    var generatedQuery = generator.stringify(parsedQuery);
+    console.log(generatedQuery);
+    queryDispatcher.query(generatedQuery).then((response: any) => console.log(response.results.bindings));
+    
+
 
     //create new Question
     // let new_question = {
