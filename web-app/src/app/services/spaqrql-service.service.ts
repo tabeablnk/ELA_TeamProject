96fb1b38646ab7ba7a383ce761a3395b1be2578a
Franziska Bumiler <franziska.bumiller@fau.de>
2022-07-16 21:50:49 +0200

    First (trivial) automatedItemGeneration - not yet working

--- web-app/src/app/services/spaqrql-service.service.ts
+++ web-app/src/app/services/spaqrql-service.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { randomInt } from 'jspsych/dist/modules/randomization';
 import * as Sparql from 'sparqljs'
 import internal from 'stream';
 import { Category } from '../models/state-enum.model';
@@ -25,8 +26,9 @@ class SPARQLQueryDispatcher {
 })
 export class SpaqrqlServiceService {
 
-  responseArrived: Boolean = false;
+  //responseArrived: Boolean = false;
   counter_SPARQL_requests = 0;
+  distractor_variability_city_questions = 2456;
 
   // Endpoint von WikiData an den die Anfrage geschickt werden muss
   endpointUrl = 'https://query.wikidata.org/sparql';
@@ -74,55 +76,67 @@ export class SpaqrqlServiceService {
   constructor(private categoryQuestions: CategoryQuestionsService) { }
 
   initGeneratedQuestions() {
-    //diese Methode wird am Anfang aufgerufen, am besten du sendest hier schon mal die SPARQUL Anfrage mit fetch
-    //über den categoryQuestinoService kannst du dann Fragen zu den verschiedenen QuestionsSets hinzufügen, wie hier zum Beispiel eine Single-Choice Frage für die Kategorie Demografie z.B.
+    //diese Methode wird am Anfang über ng_init() aufgerufen -> Initalisierung des Services + erste Anfragen verschicken
+    this.sending_request_all_cities();
 
 
-    
-
-    // installed sparqljs with npm install sparqljs (s. Notion for links to documantation)
-
-    var parsedQuery = this.parser.parse(this.prefixes_wikidata + this.sparql_query_all_cities);
-    var generatedQuery = this.generator.stringify(parsedQuery);
-    console.log(generatedQuery);
-    this.queryDispatcher.query(generatedQuery).then((response: any) => {
-      // TODO: write a mehtod that generates the question and call it here with suitable data
-      // the method should take the data from Sparql and insert into the question-queue...
-      console.log(response);
-      this.responseArrived = true;
-    });
   }
 
-  sending_request_all_cities(){
+  sending_request_all_cities(number_of_requested_citys: Number = -1){
     // Funktion wird in der internen Logik aufgerufen, um die Anfrage nach den N-größten bayrischen Städten zu stellen
     // Enthält die Generierung der Anfrage + Aufruf von fetch
 
+    // Falls keine Limitierung an Städten gesetzt wurde bzw. eine Zahl kleiner 3 eingeben wurde, 
+    // wird number_of_requested_citys per default auf 5 gesetzt 
+    if(number_of_requested_citys < 3){
+      number_of_requested_citys = 5;
+    }
 
+    var parsedQuery = this.parser.parse(this.prefixes_wikidata + this.sparql_query_all_cities);
+    var updatedQuery = parsedQuery;
+    //TODO: diese Code-Zeile zum Laufen bringen
+    //updatedQuery.limit = number_of_requested_citys;
+    
+    var generatedQuery = this.generator.stringify(updatedQuery);
+    //console.log(generatedQuery);
+
+    this.queryDispatcher.query(generatedQuery).then((response: any) => {
+      this.callback_all_cities(response.results);
+      console.log(response);
+      //this.responseArrived = true;
+    });
   }
 
-  callback_all_cities(){
+  callback_all_cities(result_sparql_request:any){
     // Funktion wird asynchron von fetch aufgerufen, sobald die Antwort auf die SPARQL-Anfrage vorhanden ist
     // Hier: Auswertung der Ergebnisse + Erstellung der Fragen aus den Ergebnissen
+    this.counter_SPARQL_requests++;
+    this.results_all_cities = result_sparql_request;
+    var counter_cities = Object.keys(result_sparql_request).length;
 
-    // Grundgerüst für eine neue Frage
-    let new_question = {
-      questionId: 0,
-      questionType: 1,
-      questionTypeName: "SingleChoice",
-      category: 1,
-      questionText: "Wo liegt die Alte Mainbrücke?",
-      imageUrl: "",
-      tip: "Hier ein Tipp",
-      answeredCorrect: false,
-      givenAnswers: ["answer1", "answer2"],
+    for (var i = 0; i < counter_cities; i++){
+      var population_of_current_city = result_sparql_request[i].population.value; 
 
-      additionalInfos: {
-        options: ["München", "Würzburg", "Nürnberg", "Erlangen", "Hof"],
-        correctAnswer: "Würzburg"
+      // Grundgerüst für eine neue Frage
+      let new_question = {
+        questionId: i+100,
+        questionType: 1,
+        questionTypeName: "SingleChoice",
+        category: 1,
+        questionText: "AIG: Wie viele Einwohner hat " + result_sparql_request[i].cityLabel.value,
+        imageUrl: "",
+        tip: "",
+        answeredCorrect: false,
+        givenAnswers: ["answer1", "answer2"],
+
+        additionalInfos: {
+          options: [population_of_current_city, population_of_current_city+this.distractor_variability_city_questions, population_of_current_city-this.distractor_variability_city_questions , population_of_current_city+2*this.distractor_variability_city_questions, population_of_current_city+3*this.distractor_variability_city_questions],
+          correctAnswer: population_of_current_city
+        }
       }
+      //TODO: Überlegen wie mit RandomNumbers mehr Varaibilität in den Options möglich ist, im Moment lösen über SideChannel möglich! (Ist immer die 4 größte, bzw. 2 kleinste Zahl)
+      this.categoryQuestions.addCategoryQuestion(Category.Demografie, new_question);
     }
-    this.categoryQuestions.addCategoryQuestion(Category.Demografie, new_question);
-
   }
 
 }
